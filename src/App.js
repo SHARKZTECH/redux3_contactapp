46ede6e8ffa2ff9c02f91babca9df77d7f89bb5a
SHARKZTECH <87173929+SHARKZTECH@users.noreply.github.com>
2022-02-28 06:15:36 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { ToastContainer } from "react-toastify";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ToastContainer />
+      <h1>Hello CodeSandbox</h1>
+    </div>
+  );
+}
